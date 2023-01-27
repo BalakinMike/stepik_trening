496dfa66431bcfddd3bfb2cd83626b5979f59814
BalakinMike <mikb2220@gmail.com>
2023-01-27 14:15:24 +0300

    set_operations

---
+++ geeky_solutions/set_operations_1.py
@@ -0,0 +1,11 @@
+# Ваша задача вывести на экран количество уникальных слов, длина которых больше 6.
+
+words = ['mention', 'soup', 'pneumonia', 'tradition', 'concert', 'tease', 'generation',
+         'winter', 'national', 'jacket', 'winter', 'wrestle', 'proposal', 'error', 
+         'pneumonia', 'concert', 'value', 'value', 'disclose', 'glasses', 'tank',
+         'national', 'soup', 'feel', 'few', 'concert', 'wrestle', 'proposal', 'soup',
+         'sail', 'brown', 'service', 'proposal', 'winter', 'jacket', 'mention', 'tradition',
+         'value', 'feel', 'bear', 'few', 'value', 'winter', 'proposal', 'government', 
+         'control', 'value', 'few', 'generation', 'service', 'national',
+         'tradition', 'government', 'mention', 'proposal']
+print(len(set([i for i in words if len(i) > 6])))
